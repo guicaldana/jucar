e5463098aa81ff14a1d3916779925eca6d36dd6f
Guilherme Teixeira Caldana <guicaldana7@gmail.com>
2024-04-16 09:11:10 -0300

    adicionando sorteador na tela inicial

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,10 +12,78 @@ export class HomeComponent implements OnInit {
         middle: false,
         right: false
     };
+    public allSaints = [
+        {
+            name: "Saint John",
+        },
+        {
+            name: "Saint Paul",
+        },
+        {
+            name: "Saint Peter",
+        },
+        {
+            name: "Saint Andrew",
+        },
+        {
+            name: "Saint James",
+        },
+        {
+            name: "Saint Matthew",
+        },
+        {
+            name: "Saint Mark",
+        },
+        {
+            name: "Saint Luke",
+        },
+        {
+            name: "Saint Jude",
+        },
+        {
+            name: "Saint Simon",
+        },
+        {
+            name: "Saint Bartholomew",
+        },
+        {
+            name: "Saint Thomas",
+        },
+        {
+            name: "Saint Philip",
+        },
+        {
+            name: "Saint James the Less",
+        },
+        {
+            name: "Saint Matthias",
+        },
+    ];
+
+    public sortedSaints = [];
+    public nonSortedSaints = [];
+
+    public sortedSaint: string;
+
+    public visibleSortedSaint: boolean;
+    public visibleSortSaintCard: boolean;
 
     focus;
     focus1;
     constructor() { }
 
-    ngOnInit() {}
+    ngOnInit() {
+        this.nonSortedSaints = this.allSaints;
+        this.visibleSortedSaint = false;
+        this.visibleSortSaintCard = false;
+    }
+
+    public sortSaint() {
+        var index = Math.floor(Math.random() * this.allSaints.length);
+        this.sortedSaint = this.nonSortedSaints[index].name;
+        this.sortedSaints.push(this.allSaints[index]);
+        this.nonSortedSaints.splice(index, 1);
+        this.visibleSortedSaint = true;
+        this.visibleSortSaintCard = true;
+    }
 }
